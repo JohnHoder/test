51d44f8bbe59c1f7d3019dd163c73fb7fdab5dc9
Erik Castricum <erikcas1972@gmail.com>
2013-01-14 12:10:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,18 @@
 
 repo sync -d -c -f -j64
 
+if [ ! -z "$GERRIT_CHANGES" ]
+then
+  export CM_EXPERIMENTAL=true
+  IS_HTTP=$(echo $GERRIT_CHANGES | grep http)
+  if [ -z "$IS_HTTP" ]
+  then
+    python $WORKSPACE/hudson/repopick.py $GERRIT_CHANGES
+    check_result "gerrit picks failed."
+  else
+    python $WORKSPACE/hudson/repopick.py $(curl $GERRIT_CHANGES)
+    check_result "gerrit picks failed."
+  fi
+fi
+
 . build/envsetup.sh && brunch $device
