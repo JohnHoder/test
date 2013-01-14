214a1694eae47693a3683e8d245a01a66020d397
Erik Castricum <erikcas1972@gmail.com>
2013-01-14 14:06:44 +0100

    update releasetypes

--- build.sh
+++ build.sh
@@ -2,6 +2,30 @@
 
 repo sync -d -c -f -j64
 
+if [ "$RELEASE_TYPE" = "CM_NIGHTLY" ]
+then
+  if [ "$REPO_BRANCH" = "gingerbread" ]
+  then
+    export CYANOGEN_NIGHTLY=true
+  else
+    export CM_NIGHTLY=true
+  fi
+elif [ "$RELEASE_TYPE" = "CM_EXPERIMENTAL" ]
+then
+  export CM_EXPERIMENTAL=true
+elif [ "$RELEASE_TYPE" = "CM_RELEASE" ]
+then
+  # gingerbread needs this
+  export CYANOGEN_RELEASE=true
+  # ics needs this
+  export CM_RELEASE=true
+fi
+
+if [ ! -z "$CM_EXTRAVERSION" ]
+then
+  export CM_EXPERIMENTAL=true
+fi
+
 if [ ! -z "$GERRIT_CHANGES" ]
 then
   git clone git://github.com/erikcas/hudson.git
