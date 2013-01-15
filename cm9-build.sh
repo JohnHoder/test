47f4f718e43a4aac7b59adccdd4e8cb5171e12b5
Erik Castricum <erikcas1972@gmail.com>
2013-01-15 16:43:59 +0100

    Update cm9-build.sh

--- cm9-build.sh
+++ cm9-build.sh
@@ -6,7 +6,7 @@ repo sync -d -c -f -j64
 echo Get CM prebuilts
 vendor/cm/get-prebuilts
 
-echo Now the real work. Let's cook something!
+echo Now the real work. Lets cook something here
 . build/envsetup.sh && brunch $device
 
 echo Doner Kebab!!!!
